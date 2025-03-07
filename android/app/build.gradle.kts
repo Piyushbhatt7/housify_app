fdd91759d03b897989ea20c5b60582d2fe7346fe
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-07 23:37:37 +0530

    gladle file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // Correct plugin usage
 }
 
 android {
     namespace = "com.example.housify_final"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -19,28 +20,32 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin") // Corrected
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.housify_final"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        //minSdk = flutter.minSdkVersion
-        minSdkVersion(23)
-        ndkVersion = "27.0.12077973"
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 24  // Corrected
+        targetSdk = 33
+        // Corrected
+        versionCode = flutter.versionCode.toInt()  // Corrected
+        versionName = flutter.versionName  // Corrected
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("debug")  // Keep this only for testing
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Add necessary dependencies here
 }
